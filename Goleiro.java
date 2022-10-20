b5a6e67d34605f45ade1d456322d38486a9e5457
ArryOliveira <113479019+ArryOliveira@users.noreply.github.com>
2022-10-20 19:35:08 -0300

    Update Goleiro.java

--- Goleiro.java
+++ Goleiro.java
@@ -12,7 +12,32 @@ public class Goleiro {
 	private int força;
 	private int equilibrio;
 	private ArrayList<Chute> listaChute=new ArrayList<Chute>();
+	private ArrayList<Integer> listaResultado=new ArrayList<Integer>();
+	public ArrayList<Integer> getListaResultado() {
+		return listaResultado;
+	}
+
+	public void setListaResultado(ArrayList<Integer> listaResultado) {
+		this.listaResultado = listaResultado;
+	}
+	private int aag;
 	
+	public int getAag() {
+		return aag;
+	}
+
+	public void setAag(int aag) {
+		this.aag = aag;
+	}
+
+	public ArrayList<Chute> getListaChute() {
+		return listaChute;
+	}
+
+	public void setListaChute(ArrayList<Chute> listaChute) {
+		this.listaChute = listaChute;
+	}
+
 	public Goleiro(int id, String nome, int velocidade, int flexibilidade, int agilidade, int coordenacao, int força,
 			int equilibrio) {
 		super();
@@ -24,19 +49,44 @@ public class Goleiro {
 		this.coordenacao = coordenacao;
 		this.força = força;
 		this.equilibrio = equilibrio;
-	}
-
-	public int AreaAtuacao() {
-		int aag;
 		aag=(int)(3*velocidade)+(2*flexibilidade)+(3*agilidade)+(2*coordenacao)+(força)+(2*equilibrio);
 		aag=(int)aag/8;
 
 		if(aag<1 && aag>16) {
 			aag=0;
 		}
-		return aag;
 	}
+	public int Pontuacao(Goleiro g) {
+		int soma=0;
+		for(int n : g.getListaResultado()) {
+			soma=soma+n;
+		}
+		return soma;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getForça() {
+		return força;
+	}
+
+	public void setForça(int força) {
+		this.força = força;
+	}
+
 	public void addChute(Chute c) {
 		listaChute.add(c);
+	}
+	public void AdicionarLista(ArrayList<Chute> lista) {
+		listaChute=lista;
+	}
+	public void AddRes (int r) {
+		listaResultado.add(r);
 	}
 }
